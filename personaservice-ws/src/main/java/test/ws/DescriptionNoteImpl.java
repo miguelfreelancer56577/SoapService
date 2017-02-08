dea0e2fc3a065da97fa034d9c09908e485464a2b
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2017-02-08 11:07:09 -0600

    I completed a web service test.

--- personaservice-ws/src/main/java/test/ws/DescriptionNoteImpl.java
+++ personaservice-ws/src/main/java/test/ws/DescriptionNoteImpl.java
@@ -5,17 +5,22 @@ import javax.jws.WebService;
 import com.mangelt.test.DescriptionNote;
 import com.mangelt.test.Employee;
 import com.mangelt.test.Note;
+import org.apache.log4j.Logger;
 
 @WebService(endpointInterface = "com.mangelt.test.DescriptionNote" , targetNamespace = "http://www.example.org/DescriptionNote/")
 public class DescriptionNoteImpl implements DescriptionNote {
 
+	final static Logger logger = Logger.getLogger(DescriptionNoteImpl.class.getName());
+	
 	@Override
 	public Note newOperation(Employee parameters) {
+		logger.debug("It is fetching Note object.");
 		Note nt = new Note();
 		nt.setBody("How are you today.");
 		nt.setFrom(parameters.getFirstname() + " " + parameters.getLastname());
 		nt.setHeading("Welcome !!!!");
 		nt.setTo("Anonymous");
+		logger.debug("Record fetched successfully.");
 		return nt;
 	}
 
