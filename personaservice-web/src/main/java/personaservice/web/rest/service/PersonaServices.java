4e4c6f6954f42e083a184e392224f957b6297ef4
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2018-02-26 17:37:26 -0600

    I've finished rest service with spring.

---
+++ personaservice-web/src/main/java/personaservice/web/rest/service/PersonaServices.java
@@ -0,0 +1,48 @@
+package personaservice.web.rest.service;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import personaservice.api.ws.User;
+import personaservice.api.ws.UserList;
+import personaservice.persistence.service.UsersService;
+
+@RestController
+public class PersonaServices {
+
+	@EJB(beanName="myUsersService")
+	UsersService usersService;
+	
+	@RequestMapping(value="/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<UserList> getAllUsers(){
+		
+		ResponseEntity<UserList> reponse = null;
+		
+		UserList userList = null;
+		
+		System.out.println("getting records");
+		
+		List<User> users = usersService.getAllusers();
+		
+		if(users != null){
+			userList = new UserList();
+			userList.setUsers(users);
+			reponse = new ResponseEntity<UserList>(userList, HttpStatus.OK);
+		}
+		
+		System.out.println("It's been gotten all records.");
+		
+		return reponse;
+		
+	}
+	
+}
