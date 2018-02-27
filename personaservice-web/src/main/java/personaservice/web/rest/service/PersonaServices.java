40807fb0d4206b4bd12ce108b52c8cb53d7b95d1
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2018-02-27 15:21:56 -0600

    it´s been finished rest service to get a user by its user and password.

--- personaservice-web/src/main/java/personaservice/web/rest/service/PersonaServices.java
+++ personaservice-web/src/main/java/personaservice/web/rest/service/PersonaServices.java
@@ -1,6 +1,7 @@
 package personaservice.web.rest.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.ejb.EJB;
 
@@ -8,10 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import personaservice.api.ws.LoginParams;
 import personaservice.api.ws.User;
 import personaservice.api.ws.UserList;
 import personaservice.persistence.service.UsersService;
@@ -25,7 +29,7 @@ public class PersonaServices {
 	@RequestMapping(value="/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<UserList> getAllUsers(){
 		
-		long threadId = Thread.currentThread().getId();
+		String threadId = UUID.randomUUID().toString();
 		
 		System.out.println("[REST][Thread] # " + threadId + " has started."); 
 		
@@ -50,5 +54,39 @@ public class PersonaServices {
 		return reponse;
 		
 	}
+	
+	@RequestMapping(value="/user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<User> getUser(@RequestBody LoginParams loginParams){
+		
+		String threadId = UUID.randomUUID().toString();
+		
+		System.out.println("[REST][Thread] # " + threadId + " has started."); 
+		
+		User user = null;
+		
+		ResponseEntity<User> reponse = null;
+		
+		System.out.println("getting record");
+		
+		user = usersService.getByLogin(loginParams.getLogin(), loginParams.getPassword());
+		
+		if(user != null){
+			reponse = new ResponseEntity<User>(user, HttpStatus.OK);
+			System.out.println("It's been gotten the record.");
+		}else {
+			
+			reponse = new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+			
+			System.out.println("There was an error to try getting the record.");
+			
+		}
+		
+		System.out.println("[REST][Thread] # " + threadId + " has finished.");
+		
+		return reponse;
+		
+	}
+
 	
 }
